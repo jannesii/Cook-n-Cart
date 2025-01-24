af2e09b8e6ff2754ba5675f4f0c1c8f53b70a5c4
jannesii <janne.siirtola@outlook.com>
2025-01-24 08:38:14 +0200

    database_test fixed

--- tests/database_test.py
+++ tests/database_test.py
@@ -1,9 +1,11 @@
 import sqlite3
 from typing import List
+import os
 
 
 def main():
-    db_path = "/utils/cook_and_cart.db"
+    db_path = "cook_and_cart.db"
+    db_path = os.path.join(os.getcwd(), "utils", db_path)
     connection = sqlite3.connect(db_path)
     connection.row_factory = sqlite3.Row
     cursor = connection.cursor()
