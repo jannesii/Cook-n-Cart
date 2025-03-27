955be663f6d7c5a50dfac9b672c10e5710eff191
jannesii <janne.siirtola@outlook.com>
2025-03-27 14:24:58 +0200

    Update buildapk.sh to use project-relative paths for wheel files

--- buildapk.sh
+++ buildapk.sh
@@ -1,5 +1,5 @@
 pyside6-android-deploy --name "Cook-n-Cart" \
-	--wheel-pyside=/home/jannesi/whl/PySide6-6.8.0.2-6.8.0-cp311-cp311-android_aarch64.whl \
-	--wheel-shiboken=/home/jannesi/whl/shiboken6-6.8.0.2-6.8.0-cp311-cp311-android_aarch64.whl \
+	--wheel-pyside=/home/jannesi/Cook-n-Cart/whl/PySide6-6.8.0.2-6.8.0-cp311-cp311-android_aarch64.whl \
+	--wheel-shiboken=/home/jannesi/Cook-n-Cart/whl/shiboken6-6.8.0.2-6.8.0-cp311-cp311-android_aarch64.whl \
 	--ndk-path=/home/jannesi/.pyside6_android_deploy/android-ndk/android-ndk-r26b/ \
 	--sdk-path=/home/jannesi/.pyside6_android_deploy/android-sdk/
