df5238bee591d338c2db6cd722d4231efc5c396d
jannesii <janne.siirtola@outlook.com>
2025-03-27 20:02:25 +0200

    Add --keep-deployment-files option to pyside6-android-deploy command

--- buildapk.sh
+++ buildapk.sh
@@ -1,4 +1,4 @@
-pyside6-android-deploy --name "Cook-n-Cart" \
+pyside6-android-deploy --name "Cook-n-Cart" --keep-deployment-files \
 	--wheel-pyside=/home/jannesi/Cook-n-Cart/whl/PySide6-6.8.0.2-6.8.0-cp311-cp311-android_aarch64.whl \
 	--wheel-shiboken=/home/jannesi/Cook-n-Cart/whl/shiboken6-6.8.0.2-6.8.0-cp311-cp311-android_aarch64.whl \
 	--ndk-path=/home/jannesi/.pyside6_android_deploy/android-ndk/android-ndk-r26b/ \
