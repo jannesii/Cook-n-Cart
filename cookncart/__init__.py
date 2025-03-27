72d221101ccd2413bcac9139ec3e493b66758311
jannesii <janne.siirtola@outlook.com>
2025-03-27 10:15:04 +0200

    Importit pois init filuista

--- cookncart/__init__.py
+++ cookncart/__init__.py
@@ -6,7 +6,7 @@ Version: 1.0.0
 __version__ = "1.0.0"
 
 # Optionally, expose frequently used components at the package level:
-from .controllers import *
-from .database import DatabaseManager
-from .models import *
-from .repositories import *
+#from .controllers import *
+#from .database import DatabaseManager
+#from .models import *
+#from .repositories import *
--- cookncart/views/__init__.py
+++ cookncart/views/__init__.py
@@ -1,8 +1,8 @@
 """
 Cookncart Views Subpackage
 """
-from .main_window import MainWindow
-from .asetukset_page import AsetuksetPage
-from .ostoslistat_page import OstoslistatPage
-from .reseptit_page import ReseptitPage
-from .tuotteet_page import TuotteetPage
+#from .main_window import MainWindow
+#from .asetukset_page import AsetuksetPage
+#from .ostoslistat_page import OstoslistatPage
+#from .reseptit_page import ReseptitPage
+#from .tuotteet_page import TuotteetPage
