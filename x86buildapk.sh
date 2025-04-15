9ed3a3e85ff5cd32f6fcb4a75ca93f21af91f0e5
jannesii <janne.siirtola@outlook.com>
2025-04-15 23:54:15 +0300

    214. VALMIS VITTU VIHDOINKIN EI MENNY KU 11h :):):):)

--- x86buildapk.sh
+++
@@ -1,5 +0,0 @@
-pyside6-android-deploy --name "CookAndCart" --keep-deployment-files \
-	--wheel-pyside=/home/jannesi/Cook-n-Cart/whl/PySide6-6.8.0.2-6.8.0-cp311-cp311-android_x86_64.whl \
-	--wheel-shiboken=/home/jannesi/Cook-n-Cart/whl/shiboken6-6.8.0.2-6.8.0-cp311-cp311-android_x86_64.whl \
-	--ndk-path=/home/jannesi/.pyside6_android_deploy/android-ndk/android-ndk-r26b/ \
-	--sdk-path=/home/jannesi/.pyside6_android_deploy/android-sdk/
