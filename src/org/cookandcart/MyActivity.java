71f7a7b86f15638cf982f25f511277668fa47597
jannesii <janne.siirtola@outlook.com>
2025-04-15 21:09:59 +0300

    214. test 7

--- src/org/cookandcart/MyActivity.java
+++ src/org/cookandcart/MyActivity.java
@@ -1,15 +1,14 @@
 package org.cookandcart.cookandcart;
 
 import org.qtproject.qt5.android.bindings.QtActivity;
-import android.widget.Toast;
+import android.util.Log;
 
 public class MyActivity extends QtActivity {
+    private static final String TAG = "MyActivity";
+
     @Override
     public void onBackPressed() {
-        console.log("Back button pressed, but doing nothing.");
-        // Do nothing; override to disable default behavior.
-        // You could add a log here or your own custom behavior if needed.
-        // For example, if you want to dismiss a keyboard or do some other logic, do it here.
-        // If you really want to completely disable the back press, just leave it empty.
+        // Do nothing (or add your own logic) to disable the default behavior.
+        Log.d(TAG, "onBackPressed() intercepted; not calling super.");
     }
 }
